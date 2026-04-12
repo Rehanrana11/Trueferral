514728209cb3865a4a2a7357de2844f5b8484eca
Rehanrana11 <Rmasood524@gmail.com>
2026-04-12 19:13:08 +0500

    fix: add revision variable to empty 001 migration

---
+++ alembic/versions/001_create_video_calls.py
@@ -0,0 +1,13 @@
+# Empty placeholder - superseded by 002_add_video_calls.py
+revision = '001_create_video_calls'
+down_revision = '0001'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
